45e91da57dc0d6ec00a6f7d91cd13a7816d30a86
Gino S <66481194+PineAppleGrits@users.noreply.github.com>
2021-11-25 22:51:37 -0300

    added cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+// next.config.js
 module.exports = {
   sassOptions: {
     includePaths: [
@@ -30,6 +30,20 @@ module.exports = {
         source: '/languages/:language',
         destination: '/?language=:language',
         permanent: true
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
       }
     ]
   }
